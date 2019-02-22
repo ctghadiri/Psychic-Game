b2879a16f5282480b2c3a406c91245d8f2905b01
ctghadiri <ctghadiri@gmail.com>
2019-02-22 15:10:46 -0800

    add list of used letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var computerOptions = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 // create array for user previous guesses
-var userAttempts = [""]
+var userAttempts = []
 
 // establish variables
 var wins = 0;
@@ -11,9 +11,11 @@ var guesses = 9;
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var userChoiceText = document.getElementById("userchoice-text");
+var userAttemptsText = document.getElementById("attempts-text")
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessText = document.getElementById("guess-text")
+var directionsText = document.getElementById("directions-text");
 
 // -------------------------------------------
 
@@ -23,43 +25,49 @@ var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
     // prevent other key hits from affecting game
-        computerOptions.indexOf(userGuess) > -1
+        directionsText.textContent = ""
 
         // Determines which key was pressed.
         var userGuess = event.key;
-        //push to empty array to prevent choices
-        var x =  userAttempts.push(userGuess)
-    
-// create if statement for when you choose right
-    // computer chooses new number ??????
-    if (userGuess){
+        
+        // create if statement for when you choose right
+        // computer chooses new number ??????
+        if (computerOptions.indexOf(userGuess) > -1){
+        
+            if (userAttempts.includes(userGuess) === false) {
+                userChoiceText.textContent = "You chose: " + userGuess; 
 
-    
-        if (userGuess === computerChoice) {
-            alert("WINNER WINNER CHICKEN DINNER!")
-            wins = wins + 1;
-            guesses = 9;
-            userAttempts.length = 0
+                userAttempts.push(userGuess)
+        
+                if (userGuess === computerChoice) {
+                    alert("WINNER WINNER CHICKEN DINNER!")
+                    wins = wins + 1;
+                    guesses = 9;
+                    userAttempts.length = 0
+                    computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+                }
+                // create if statment for when you choose wrong
+                    // decrease guesses
+                        // if guesses < 0 reset game (firgure out how it chooses new number after loss or win)
+                else {
+                    guesses = guesses - 1; 
+
+                    if ( guesses === 0){
+                        alert("YOU LOSE");
+                        losses = losses + 1;
+                        guesses = 9;
+                        userAttempts.length = 0
+                        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+                    }
+                }
+            }    
         }
-        // create if statment for when you choose wrong
-            // decrease guesses
-                // if guesses < 0 reset game (firgure out how it chooses new number)
-        else {
-            guesses = guesses - 1; 
 
-            if ( guesses === 0){
-                alert("YOU LOSE");
-                losses = losses + 1;
-                guesses = 9;
-                userAttempts.length = 0
-            }
-        }
-    }
+        // display wins and losses and user choice
 
-// display wins and losses and user choice
-
-        userChoiceText.textContent = "You chose: " + userGuess;
+        
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
-        guessText.textContent = "Guesses" + guesses;
+        guessText.textContent = "Guesses: " + guesses;
+        userAttemptsText.textContent = "Attempts: " + userAttempts;
 }
